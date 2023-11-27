ec7ecb24c2eb3205eb53cca2ac5062d1106bc215
nemat <nematsiddiqui@gmail.com>
2023-11-27 22:39:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/twitter", (req, res) => {
+  res.send("nematsiddiquiatgmail.com");
+});
+app.get("/login", (req, res) => {
+  res.send("<h1>Please login at chai aur code </h1>");
+});
+app.get("/youtube", (req, res) => {
+  res.send("<h2>Chai aur code </h2>");
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
